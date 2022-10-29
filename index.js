16bd89d4d962199c6d509f3669a312776159e798
Ryan Fitzpatrick <ryantfitzpatrick@yahoo.com>
2022-10-29 16:16:57 -0400

    commit tree c648ee710a9cd840ca03852269d72a9244b4580a

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+
+const num3 = 64;
+const num4 = 16;
+const mod = num3 / num4;
+
+
+
+const max = Math.max(1, 2, 10, 20);
